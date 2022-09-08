d43535fa2233fbfadd18db9d03c1a925b9876cb7
Tobi <malex.josh@gmail.com>
2022-09-08 10:18:16 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include<stdio.h>
+
+/**
+* main - like a room where programs work
+* printf: to output a text, expression etc
+* Return: always 0 (that is return nothing)
+*/
+
+int main(void)
+{
+printf("Size of a char: %i byte(s)", sizeof(char));
+printf("Size of a char: %i byte(s)", sizeof(int));
+printf("Size of a char: %i byte(s)", sizeof(long int));
+printf("Size of a char: %i byte(s)", sizeof(long long int));
+return (0);
+}
