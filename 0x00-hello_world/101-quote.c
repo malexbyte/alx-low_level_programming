7fe25fa6340d3ee276e5cdd13f4372917e8adafa
Tobi <malex.josh@gmail.com>
2022-09-08 12:19:54 -0700

    adv. task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-fwrite("\n", stderr);
+fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
