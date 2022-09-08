ba9c502a277cc8ff92bdc1019837e69f6e45abb8
Tobi <malex.josh@gmail.com>
2022-09-08 11:55:09 -0700

    adv. task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - like a room where programs work
+ * printf: to output a text, expression etc
+ * Return: return 1 (that is return nothing)
+ */
+
+int main(void)
+{
+printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fwrite("\n", stderr);
+return (1);
+}
