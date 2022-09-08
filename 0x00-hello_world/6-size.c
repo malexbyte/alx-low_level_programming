0d7239d7ea104f33d5693458ba3815d36d36c80e
Tobi <malex.josh@gmail.com>
2022-09-08 10:35:27 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
 * main - like a room where programs work
 * printf: to output a text, expression etc
 * Return: always 0 (that is return nothing)
 */
-
 int main(void)
 {
 printf("Size of a char: %i byte(s)\n", sizeof(char));
