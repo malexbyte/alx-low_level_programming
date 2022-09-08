bb35800c822b3b39cbd9e44302b130bdaf6aa9da
Tobi <malex.josh@gmail.com>
2022-09-08 12:29:43 -0700

    adv. task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - like a room where programs work
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-write("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
