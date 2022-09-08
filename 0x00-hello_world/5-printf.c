b80b6b6de7689002d7b8d53b6c3171981836d30e
Tobi <malex.josh@gmail.com>
2022-09-08 09:55:39 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include<stdio.h>
+
+/**
+* main - like a room where programs work
+* printf: to output a text, expression etc
+* Return: always 0 (that is return nothing)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
