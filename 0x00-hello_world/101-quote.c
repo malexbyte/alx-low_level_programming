d17da768d32ab79e02c76244a386b2ad04acb9d7
Tobi <malex.josh@gmail.com>
2022-09-08 12:56:18 -0700

    adv. task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - like a room where programs work
@@ -9,6 +8,7 @@
 
 int main(void)
 {
-fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+char e = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+fwrite(stderr, "%s", e);
 return (1);
 }
