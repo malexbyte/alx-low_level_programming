2135e75c8a7cc18f5279acecd52cc03b315eb5f9
Tobi <malex.josh@gmail.com>
2022-09-08 07:03:00 -0700

    some changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+/**
+ * main - like a room where programs work
+ * Return: always 0 (that is return nothing)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
