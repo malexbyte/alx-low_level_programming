054a5d91e7686f641b3a367070a92f863eba6021
Tobi <malex.josh@gmail.com>
2022-09-09 07:33:39 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
-#include <stdio.h>                                                                                                              
-/**                                                                                                                             
- * main - entry point                                                                                                           
- *                                                                                                                              
- * Return: alway returns 0                                                                                                      
- *                                                                                                                              
- */                                                                                                                             
-int main(void)                                                                                                                  
-{                                                                                                                               
-        char a = 'a';                                                                                                                 
-                                                                                                                                
-        while (a <= 'z')
-        {
-            putchar(a);
-            a++;
-        }                                                                                                                                                                                                
-        putchar('\n');                                                                                                          
-        return (0);
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * 
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
 }
