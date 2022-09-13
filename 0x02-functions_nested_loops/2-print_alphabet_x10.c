47152e1ad8eeef9ee017e5da4673c1f4b1a7f6c4
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-13 15:03:38 +0100

    titss

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-    int a;
-    int i;
-    for (i = 1; i <= 10;)
-	{
-		for (a = 'a'; a <= 'z'; a++)
-		{
-			_putchar(a);
-		}
-        i++;
-		
-        _putchar('\n');
-	}
+int a;
+int i;
+for (i = 1; i <= 10;)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+i++;
+	
+_putchar('\n');
+}
 
 }
