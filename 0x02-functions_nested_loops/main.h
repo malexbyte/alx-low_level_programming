b85a8bb4631c83dc5873b23500328f0aaf9c783c
Tobi <malex.josh@gmail.com>
2022-09-13 02:26:55 -0700

    the updated header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+* _putchar - writes the character c to std
+* c: parameter to compare
+* Return: 1 or 0
+*/
+
+int _putchar(char *c);
+/**
+ * int - a data type to specify the kind of value that is used in the function
+ * _putchar - the "_" before the putchar is used when declaring a fuction
+ * (char *c) - is the arguments passed
+ */
+#endif
