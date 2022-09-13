5ee091d9b0e039d33d66a778bee95a08d4fd233d
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-13 23:08:40 +0100

    kjh

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int a;
-	int b = 0;
+int a;
+int b = 0;
 
-	for (a = 0; a < 1024; a++)
-	{
-		if (a % 3 == 0 || a % 5 == 0)
-		{
-			b += a;
-		}
+for (a = 0; a < 1024; a++)
+{
+if (a % 3 == 0 || a % 5 == 0)
+{
+b += a;
+}
 
-	}
-	printf("%d\n", b);
-	return (0);
+}
+printf("%d\n", b);
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -59,4 +59,5 @@ int print_last_digit(int a);
  void times_table(void);
  int add(int, int);
  void print_to_98(int n);
+ void print_times_table(int n);
 #endif
