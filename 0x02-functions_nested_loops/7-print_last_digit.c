14a0c1fe4471ba5f9d5305f1c59d77e3c85d1ee8
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-13 22:11:02 +0100

    jh

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
  * print_last_digit - is a function name
- * @n: the int variable
+ * @a: the int variable
  * Return 0 - returns a value
  */
 
-int print_last_digit(int n)
+int print_last_digit(int a)
 
 {
 int last;
 
-n = (int)(n);
-if (n < 0)
-n = (-1) * n;
-last = n % 10;
+a = (int)(a);
+if (a < 0)
+a = (-1) * a;
+last = a % 10;
 if ((last + '0') == '(')
 last = 8;
 _putchar(last + '0');
+
 return (last);
 
 }
