63de56631f65beca1acafeb124a7a85a64fc9dc1
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-13 21:59:16 +0100

    hg

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,15 @@
 
 int print_last_digit(int n)
 {
-n = (-1) * n;
-n = n % 10;
-_putchar('0' + (n % 10));
-return (n);
+int last;
+
+	n = (int)(n);
+	if (n < 0)
+		n = (-1) * n;
+	last = n % 10;
+	if ((last + '0') == '(')
+		last = 8;
+	_putchar(last + '0');
+	return (last);
 
 }
