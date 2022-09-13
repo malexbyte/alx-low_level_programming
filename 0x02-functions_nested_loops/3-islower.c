56cc7548fa9db1894dd7ac2d9c179f8ad53f5c2a
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-13 15:27:19 +0100

    dd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet - its a function in the main header that allows us to print
- *
+ * _islower - its a function in the main header that allows us to print
+ * int c - an integer argument 
  * Return: Always returns 0
  */
 
 int _islower(int c)
 {
-if(c >= 'a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 return (c);
 _putchar('\n');
 return (0);
