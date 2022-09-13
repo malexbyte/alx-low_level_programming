fc9bb07ec29c93451a1a3785436ddfbc52abcb30
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-13 12:27:51 +0100

    yeah

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,14 @@
 
 int main(void)
 {
-char *a = '_putchar'; 
-_putchar(*a);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@
 * Return: 1 or 0
 */
 
-int _putchar(char *c);
+int _putchar(char c);
 /**
  * int - a data type to specify the kind of value that is used in the function
  * _putchar - the "_" before the putchar is used when declaring a fuction
