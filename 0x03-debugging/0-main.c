ac1134e3a31184e1c321cad4f69b865fdc7027d1
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-14 13:43:09 +0100

    drtyhg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
-* main - tests function
-* Return: 0
+* main - prints integer
+* return - 0
 */
 
 int main(void)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
 
