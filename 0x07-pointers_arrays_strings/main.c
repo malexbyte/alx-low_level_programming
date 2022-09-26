f2cc3cf66931b371fd89d23a7f2ca3b84f1c57d6
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-26 11:01:10 +0100

    12

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,91 +1,79 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdio.h>
 
 
 
 /**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- *  * simple_print_buffer - prints buffer in hexa
- *
- *   * @buffer: the address of memory to print
- *
- *    * @size: the size of the memory to print
- *
- *     *
- *
- *      * Return: Nothing.
- *
- *       */
-
+ * Return: Nothing.
+ */
 void simple_print_buffer(char *buffer, unsigned int size)
 
 {
 
-	        unsigned int i;
+        unsigned int i;
 
 
 
-		        i = 0;
+        i = 0;
 
 			        while (i < size)
 
-					        {
+        {
 
-							                if (i % 10)
+                if (i % 10)
 
-										                {
+                {
 
-													                        printf(" ");
+                        printf(" ");
 
-																                }
+                }
 
-									                if (!(i % 10) && i)
+                if (!(i % 10) && i)
 
-												                {
+                {
 
-															                        printf("\n");
+                        printf("\n");
 
-																		                }
+                }
 
-											                printf("0x%02x", buffer[i]);
+                printf("0x%02x", buffer[i]);
 
-													                i++;
+                i++;
 
-															        }
+        }
 
-				        printf("\n");
+        printf("\n");
 
 }
 
 
 
 /**
+ * main - check the code
  *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
 
 {
 
-	    char buffer[98] = {0x00};
+    char buffer[98] = {0x00};
 
 
 
-	        simple_print_buffer(buffer, 98);
+    simple_print_buffer(buffer, 98);
 
-		    _memset(buffer, 0x01, 95);
-
-		        printf("-------------------------------------------------\n");
-
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
 			    simple_print_buffer(buffer, 98);    
-
-			        return (0);
+    simple_print_buffer(buffer, 98);    
+			    simple_print_buffer(buffer, 98);    
+    simple_print_buffer(buffer, 98);    
+    return (0);
 
 }
