3de4548e339a6fd888b2f908eadc73c9bfe99092
malexbyte <61620247+malexbyte@users.noreply.github.com>
2022-09-30 13:32:33 +0100

    sssss

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,51 +1,35 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * main - print the minimum number of coins to make change
+ * @argc: int
+ * @argv: string
+ * Return: int
  */
-int main(int argc, char  *argv[])
+
+int main(int argc, char *argv[])
 {
-	int coins = 0;
+	int i, n, s, ch = 0;
+	int c[] = {25, 10, 5, 2, 1};
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		if (strchr(argv[argc - 1], '-'))
-		{
-			printf("0\n");
-			return (1);
-		}
-		int money;
-
-		money = atoi(argv[argc - 1]);
-
-		while (money > 0)
-		{
-			if (money % 25 == 0)
-			{
-				money -= 25;
-			} else if (money % 10 == 0)
-			{
-				money -= 10;
-			} else if (money % 5 == 0)
-			{
-				money -= 5;
-			} else if (money % 2 == 0)
-			{
-				money -= 2;
-			} else
-			{
-				money--;
-			}
-			coins++;
-		}
-		printf("%d\n", coins);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	printf("Error\n");
-	return (1);
+	s = atoi(argv[1]);
+	if (s < 1)
+		printf("0\n");
+	else
+	{
+		for (i = 0; i < 5 && s; i++)
+		{
+			n = s / c[i];
+			ch += n;
+			s -= n * c[i];
+		}
+		printf("%d\n", ch);
+	}
+	return (0);
 }
